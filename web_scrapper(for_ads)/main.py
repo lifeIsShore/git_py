aa1e01a6802a69749537bc4ab6ed4c9564812523
LifeIsShore <ahmtyagmur03@gmail.com>
2024-11-09 23:15:36 +0100

    fix the deliminators as ; it is fixed now

--- web_scrapper(for_ads)/main.py
+++ web_scrapper(for_ads)/main.py
@@ -11,7 +11,8 @@ def process_urls_from_csv(input_csv, output_dir):
         output_dir (str): Directory path where the CSV and log files will be saved.
     """
     with open(input_csv, mode='r', newline='', encoding='utf-8-sig') as file:
-        reader = csv.reader(file)
+        reader = csv.reader(file, delimiter=';')  # Delimiter olarak `;` belirtiyoruz
+        next(reader, None)  # Optional: Skip the header row if your CSV has one
         for row in reader:
             url = row[-1]  # URL is assumed to be in the last column
             print(f"Processing URL: {url}")
@@ -23,7 +24,6 @@ def process_urls_from_csv(input_csv, output_dir):
 
     # Print message when all URLs have been processed
     print("Finished processing all URLs.")
-
 
 # CSV dosyasının konumu ve çıktı klasörünün yolu
 input_csv = r"C:\Users\ahmty\Desktop\Python\geo_DSproject_github_clone\git_py\csv_input\immowelt_urls_copy (used in web scraping).csv"
