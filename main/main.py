0ce58a322cdd2b082e3181c5159f79743ded4098
LifeIsShore <ahmtyagmur03@gmail.com>
2024-11-09 01:34:06 +0100

    ok (comments are added)

--- main/main.py
+++ main/main.py
@@ -1,1 +1,11 @@
 from web_scrapper import  scrape_immowelt, setup_logging, sanitize_filename 
+
+''' 
+loop ile immowelt_urls_copy (used in web scraping).csv masaüstü bu dosyadan cekecegim 
+url leri teker tker basacak scrapping yapcak ve bunlari kayit edecek. ben de buna bir istatistik uygulayacagim 
+ve sehir konumundan bir agirlik vereegim (ayni özelliktei evleri fiyet olaarak karsilatirarak.).
+sonra bu agirlik her bir sehir icin eslesecek ve ayrica bu geospatial konumu da alip esleyecegim.
+sonrasinda bunlari ilanlar icin olusturdugum db ye aktaracagim.
+ve tekrardan bu db nin constraintslerini geri yükleyecegim.
+
+'''
