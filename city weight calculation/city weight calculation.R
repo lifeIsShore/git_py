8c651b685c50854881ed95a3743c681775f2e315
LifeIsShore <ahmtyagmur03@gmail.com>
2024-11-11 16:23:59 +0100

    csv s are created

--- city weight calculation/city weight calculation.R
+++ city weight calculation/city weight calculation.R
@@ -8,13 +8,17 @@ log_weights <- log(price_per_sqm)
 # 2. Min-Max normalization
 log_weights_normalized <- (log_weights - min(log_weights)) / (max(log_weights) - min(log_weights))
 
+# here Z score scaling:
+log_weights_standardized_Z <- scale(log_weights)
+
 # Display the results
 data.frame(
-  City_ID = 1:length(price_per_sqm),
   Price_per_sqm = price_per_sqm,
-  Log_Weight = log_weights,
-  Normalized_Weight = log_weights_normalized
+  Normalized_Weight = log_weights_normalized,
+  log_weights_standardized_Z
 )
 
 # if 0 is a value of one of city it is not a demonstration of absence, 
 # since ML model will interepret that as a weighted city score 
+
+
